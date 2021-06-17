fc480948c95ade94c6d32ba88f4e07a7b3daa4d8
stephappiah <appstephen8@gmail.com>
2021-06-17 15:19:33 +0000

    authorization for product endpoints

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,25 +1,36 @@
 from rest_framework import generics
-from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
-
+from rest_framework.permissions import IsAuthenticated
 
 
 class ProductCreateApi(generics.CreateAPIView):
+
+    permission_classes = [IsAuthenticated]
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductListApi(generics.ListAPIView):
+
+    permission_classes = [IsAuthenticated]
+
     queryset = Product.objects.all()
     serializer_class =ProductSerializer
 
 
 class ProductUpdateApi(generics.RetrieveUpdateAPIView):
+
+    permission_classes = [IsAuthenticated]
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
 class ProductDeleteApi(generics.DestroyAPIView):
+
+    permission_classes = [IsAuthenticated]
+
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
